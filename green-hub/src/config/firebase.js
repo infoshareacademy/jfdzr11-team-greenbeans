a58bddd104de303f45e22f5906dd71b22b549078
PatrykUminski <phantomhd2@gmail.com>
2023-06-19 19:40:02 +0200

    feat: Firebase init

---
+++ green-hub/src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Ska1ZKPhjPtnMQiVhRNdFjVdj_8tscQ",
+  authDomain: "greenhub-66a3d.firebaseapp.com",
+  projectId: "greenhub-66a3d",
+  storageBucket: "greenhub-66a3d.appspot.com",
+  messagingSenderId: "175021899961",
+  appId: "1:175021899961:web:6356ed9e7eb9ca1a88db68",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
