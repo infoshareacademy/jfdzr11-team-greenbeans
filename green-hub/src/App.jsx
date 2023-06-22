3f9ce96ac869b82fd0a57935e1e2c83ea265bcd7
Zuza <zuzia.melaniuk@gmail.com>
2023-06-22 22:55:25 +0200

    App - basic changes in css, ideas routing

--- green-hub/src/App.jsx
+++ green-hub/src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import styles from "./App.module.css"
+import { Ideas } from "./components";
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-	return <h1>Welcome to GreenHub</h1>;
+  return (
+	// <h1>Welcome to GreenHub</h1>
+    <Routes>
+		  <Route path="/newideas" element={<Ideas/>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
