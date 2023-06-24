2244bd0946d8b600b5893ca4e8b764107bfc2c73
Beata <bearadkiewicz@gmail.com>
2023-06-24 08:49:40 +0200

    routing added

---
+++ green-hub/src/components/Articles/Articles.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Articles.module.css";
+import { Routes, Route } from "react-router-dom";
+
+
+export const Articles = () => {
+  return (
+    <div>Articles</div>
+  )
+}
+  
+
