4301fef07dedb04b5c97c12b1968078d2546e58f
Beata <bearadkiewicz@gmail.com>
2023-06-24 08:50:17 +0200

    routing

--- green-hub/src/App.jsx
+++ green-hub/src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { Articles } from "../src/components/Articles/Articles";
+import { Routes, Route } from "react-router-dom";
+import { ReactDOM } from "react";
+
 
 function App() {
-	return <h1>Welcome to GreenHub</h1>;
+	
+	return (
+		<div>
+			<h1>Welcome to GreenHub</h1>
+		<Routes>
+				<Route path="/articles" element={<Articles/>}/>
+
+				<Route path="/" element={<Home />}/>
+				<Route path="/login" element={<Login/>}/>
+				<Route path="/register" element={<Register/>}/>
+				
+				<Route path="/getpoints" element={<GetPoints/>}/>
+				<Route path="/prizes" element={<Prizes/>}/>
+				<Route path="/newideas" element={<NewIdeas/>}/>
+				
+		</Routes>
+		</div>
+	);
+	
 }
 
 export default App;
