06890647d525e90279b36f0653563303ea732caa
Zuza <zuzia.melaniuk@gmail.com>
2023-06-27 00:26:49 +0200

    Components - Navbar modified

--- green-hub/src/components/Navbar/Navbar.jsx
+++ green-hub/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+	const {path, setPath} = useState()
+
+	const pathname = window.location.pathname
+	console.log(pathname)
+
+	// const 
+
 	return (
 		<div className={styles.navbar}>
 			<ul>
-				<li>articles</li>
-				<li>points</li>
-				<li>new ideas</li>
-				<li>prize</li>
+				<Link to="/">GH</Link>
+				<Link to="/articles"><img src="../../../assets/images/page-main/copywriting.png"/></Link>
+				<Link to="/getpoints"><img src="../../../assets/images/page-main/heart.png"/></Link>
+				<Link to="/newideas"><img src="../../../assets/images/page-main/idea.png"/></Link>
+				<Link to="/prizes"><img src="../../../assets/images/page-main/trophy.png"/></Link>
 			</ul>
 			<div>
 				<p>Your points: 750</p>
