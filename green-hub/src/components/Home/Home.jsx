349104966d5ef30051d9f9125f62b49a2c9c036a
Michał Ostrowski <ostmic@gmail.com>
2023-07-01 09:17:47 +0200

    registered user id == collection document id

--- green-hub/src/components/Home/Home.jsx
+++ green-hub/src/components/Home/Home.jsx
@@ -5,10 +5,10 @@ import copywriting from "../../../assets/images/page-main/copywriting.png";
 import heart from "../../../assets/images/page-main/heart.png";
 import idea from "../../../assets/images/page-main/idea.png";
 import trophy from "../../../assets/images/page-main/trophy.png";
-import cloud from '../../../assets/images/page-main/cloud.png';
-import bad from '../../../assets/images/backgrounds/help.png'
-import semi from '../../../assets/images/backgrounds/better2.png'
-import good from '../../../assets/images/backgrounds/thebest.png'
+import cloud from "../../../assets/images/page-main/cloud.png";
+import bad from "../../../assets/images/backgrounds/help.png";
+import semi from "../../../assets/images/backgrounds/better2.png";
+import good from "../../../assets/images/backgrounds/thebest.png";
 
 import { getDocs, collection } from "@firebase/firestore";
 import { db } from "../../config/firebase";
@@ -16,27 +16,29 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
 	const { logout, currentUser } = useAuth();
-	const [user, setUser] = useState('')
-	const usersCollectionRef = collection(db, "users")
+	const [user, setUser] = useState("");
+	const usersCollectionRef = collection(db, "users");
 
 	const getUser = async () => {
 		try {
-		  const data = await getDocs(usersCollectionRef);
-		  const filteredData = data.docs.map((doc) => ({
-			...doc.data()
-		  }))
-		  const user = filteredData.filter(user => user.email === currentUser.email)
-		  const userName = `${user[0].name}`
-		  console.log(filteredData)
-		  setUser(userName);
+			const data = await getDocs(usersCollectionRef);
+			const filteredData = data.docs.map((doc) => ({
+				...doc.data(),
+			}));
+			const user = filteredData.filter(
+				(user) => user.email === currentUser.email
+			);
+			const userName = `${user[0].name}`;
+			console.log(filteredData);
+			setUser(userName);
 		} catch {
-		  console.log("no user here");
+			console.log("no user here");
 		}
-	  }
-	  
-	  useEffect(() => {
+	};
+
+	useEffect(() => {
 		getUser();
-	  }, [])
+	}, []);
 
 	const handleLogout = async () => {
 		try {
@@ -49,33 +51,37 @@ const Home = () => {
 
 	const getBackgroundImage = () => {
 		if (pointsTotal >= 1500) {
-			return good}
-			else if (pointsTotal >= 1000 && pointsTotal < 1500) {
-				return semi}
-				else {
-					return bad
-				}
-			};
+			return good;
+		} else if (pointsTotal >= 1000 && pointsTotal < 1500) {
+			return semi;
+		} else {
+			return bad;
+		}
+	};
 
 	return (
-		<div className={styles.home_container} style={{backgroundImage: `url(${getBackgroundImage()})`}}>
-			<Link to="/">
-				<h1>Welcome to GreenHub</h1>
-			</Link>
+		<div
+			className={styles.home_container}
+			style={{ backgroundImage: `url(${getBackgroundImage()})` }}
+		>
 			<div className={styles.login_holder}>
 				{!currentUser ? (
 					<>
 						<Link to="/login">
-							<button className={styles.loginbtn}
-							style={{ backgroundImage: `url(${cloud})` }}>
+							<button
+								className={styles.loginbtn}
+								style={{ backgroundImage: `url(${cloud})` }}
+							>
 								Log In
 							</button>
 						</Link>
 						<Link to="/register">
-							<button className={styles.registerbtn}
-							style={{ backgroundImage: `url(${cloud})` }}>
+							<button
+								className={styles.registerbtn}
+								style={{ backgroundImage: `url(${cloud})` }}
+							>
 								Register
-								</button>
+							</button>
 						</Link>
 					</>
 				) : (
@@ -91,34 +97,56 @@ const Home = () => {
 			</div>
 			<div className={styles.text_holder}>
 				{!currentUser ? (
-					<><h1>Hello friend!</h1>
-					<h2>It's nice to meet You!</h2></>
-				) : (<><h1>Hello {`${user}`}!</h1>
-				<h2>It's a pleasure to see U again!</h2></>)}
+					<>
+						<h1>Hello friend!</h1>
+						<h2>It's nice to meet You!</h2>
+					</>
+				) : (
+					<>
+						<h1>Hello {`${user}`}!</h1>
+						<h2>It's a pleasure to see U again!</h2>
+					</>
+				)}
 				<p> Let's help our planet!</p>
 			</div>
 			<div className={styles.button_holder}>
 				<Link to="/articles">
-					<button className={styles.btns}>Articles
-					<img className={styles.icon} src={copywriting}></img></button>
+					<button className={styles.btns}>
+						Articles
+						<img
+							className={styles.icon}
+							src={copywriting}
+						></img>
+					</button>
 				</Link>
 				<Link to="getpoints">
-					<button className={styles.btns}>Get points!
-					<img className={styles.icon} src={heart}></img>
+					<button className={styles.btns}>
+						Get points!
+						<img
+							className={styles.icon}
+							src={heart}
+						></img>
 					</button>
 				</Link>
 				<Link to="newideas">
-					<button className={styles.btns}>New ideas
-					<img className={styles.icon} src={idea}></img>
+					<button className={styles.btns}>
+						New ideas
+						<img
+							className={styles.icon}
+							src={idea}
+						></img>
 					</button>
 				</Link>
 				<Link to="/prizes">
-					<button className={styles.btns}>Prizes
-					<img className={styles.icon} src={trophy}></img>
+					<button className={styles.btns}>
+						Prizes
+						<img
+							className={styles.icon}
+							src={trophy}
+						></img>
 					</button>
 				</Link>
 			</div>
-			
 		</div>
 	);
 };
