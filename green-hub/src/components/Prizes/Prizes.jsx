82dc20b070231c3acc8d7c22dd5efa1920507fa8
Zuza <zuzia.melaniuk@gmail.com>
2023-07-01 16:39:29 +0200

    Components - Modal in Prizes section added

--- green-hub/src/components/Prizes/Prizes.jsx
+++ green-hub/src/components/Prizes/Prizes.jsx
@@ -1,47 +1,37 @@
-import React, { useState } from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import styles from './Prizes.module.css'
-import PrizesBox from '../PrizesBox/PrizesBox'
-import data from './PrizesData.json'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import styles from "./Prizes.module.css";
+import PrizesBox from "../PrizesBox/PrizesBox";
+import data from "./PrizesData.json";
+
 
 const Prizes = () => {
 
-  const [visible, setVisible] = useState(false)
-  // const [disabled, setDisabled] = useState(false)
-
-  // const visible = () => {
-
-  // }
-
   return (
     <div className={styles.prizes}>
-        <Navbar/>
-        <main>
-          <h1>prize</h1>
-          <p>it's win win situation, look what we prepared for U</p>
-          <div className={styles.prizeBox}>
-            {data.map((prize, index) => {
-              return (
-                <PrizesBox 
+      <Navbar />
+      <main>
+        <h1>prize</h1>
+        <p>it's win win situation, look what we prepared for U</p>
+        <div className={styles.prizeBox}>
+          {data.map((prize, index) => {
+            return (
+              <PrizesBox
                 key={index}
                 logo={prize.logo}
                 prize={prize.prize}
                 points={prize.points}
                 value={prize.value}
-                getPrize={() => {setVisible(true); console.log("działa")}}/>
-              )
-            })}
-          </div>
-          {visible === true ? <section className={styles.info}>
-          <button onClick={() => {setVisible(false)}}>✖</button>
-            <p>Here will be Your Prize</p>
-            </section> : null}
-          
-        </main>
-        <Footer />
+                shop={prize.shop}
+              />
+            );
+          })}
+        </div>
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Prizes
+export default Prizes;
--- green-hub/src/components/PrizesBox/PrizesBox.jsx
+++ green-hub/src/components/PrizesBox/PrizesBox.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./PrizesBox.module.css";
 import useAuth from "../../context/AuthContext";
+import { v4 as uuidv4 } from "uuid";
+import Modal from "react-modal";
+import { useState } from "react";
 
-const PrizesBox = ({ logo, prize, points,getPrize }) => {
+const PrizesBox = ({ logo, prize, points, shop}) => {
+
+  const [visible, setVisible] = useState(false);
+
+  function closeBox() {
+    setVisible(false);
+  }
 
   const userPoints = 600;
   const {currentUser} = useAuth()
@@ -10,11 +19,31 @@ const PrizesBox = ({ logo, prize, points,getPrize }) => {
   return (
     <>
       {currentUser && userPoints >= points ? (
-        <button className={styles.box} onClick={getPrize}>
+        <>
+        <button className={styles.box} onClick={() => {setVisible(true)}}>
           <img src={logo} />
           <span>{prize}</span>
           <span className={styles.points}>{points} points</span>
         </button>
+        <Modal
+        isOpen={visible}
+        onRequestClose={closeBox}
+        className={styles.info}
+      >
+        <button className={styles.cancel} onClick={closeBox}>✖</button>
+        <img src="../../../assets/images/page-main/trophy.png" />
+        
+        <h3>{prize} {shop}</h3>
+        <p>points: {points}</p>
+        <div>
+        <p>activate your prize here: </p>
+        <button>ACTIVATE</button>
+        </div>
+        
+        {/* <p>{uuidv4()}</p> */}
+        
+      </Modal>
+      </>
       ) : (
         <button className={`${styles.box} ${styles.disabled}`}>
           <img src="../../../assets/images/page-prize/lock.png" className={styles.lock} />
