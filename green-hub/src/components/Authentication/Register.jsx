9cf5886a50ba8741a5bebafb39498acc7d386d2b
Michał Ostrowski <ostmic@gmail.com>
2023-07-02 10:30:59 +0200

    Loader in Login and Register

--- green-hub/src/components/Authentication/Register.jsx
+++ green-hub/src/components/Authentication/Register.jsx
@@ -5,10 +5,14 @@ import useAuth from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { setDoc, doc } from "@firebase/firestore";
 import { db } from "../../config/firebase";
+import { useState } from "react";
+import Loader from "../Loader/Loader";
 
 const Register = () => {
 	const navigate = useNavigate();
 	const { register } = useAuth();
+	//stan ładowania do implementacji loadera
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
@@ -23,6 +27,7 @@ const Register = () => {
 			try {
 				//zmienna do przechwycenia id użytkownika z autentykacji
 				const registeredUser = await register(email, password);
+				setIsLoading(true);
 				//Dodanie danych do kolekcji users z własnym id
 				await setDoc(doc(db, "users", registeredUser.user.uid), {
 					name: event.target?.firstName.value,
@@ -32,6 +37,9 @@ const Register = () => {
 					points: 0,
 					pointsTotal: 0,
 				});
+
+				setIsLoading(false);
+
 				navigate("/");
 				toast.success("Sucessfully registered");
 			} catch (error) {
@@ -42,61 +50,68 @@ const Register = () => {
 	};
 
 	return (
-		<div className={styles.layout}>
-			<div className={styles.auth_container}>
-				<h1>
-					Hi! I'm plant{" "}
-					<img src="../../../assets/images/page-main/plant-11.png" />,<br></br>
-					let introduce yourself!
-				</h1>
-				<form
-					onSubmit={handleSubmit}
-					className={styles.auth_form}
-				>
-					{/* <label htmlFor="firstName">Name</label> */}
-					<input
-						type="text"
-						name="firstName"
-						id="firstName"
-						placeholder="what's your name?"
-						required
-					/>
-					{/* <label htmlFor="lastName">Lastname</label> */}
-					<input
-						type="text"
-						name="lastName"
-						id="lastName"
-						placeholder="what's your lastname?"
-						required
-					/>
-					{/* <label htmlFor="email">Email</label> */}
-					<input
-						type="email"
-						name="email"
-						id="email"
-						placeholder="enter your email"
-						required
-					/>
-					{/* <label htmlFor="password">Password</label> */}
-					<input
-						type="password"
-						name="password"
-						id="password"
-						placeholder="create password"
-						required
-					/>
-					{/* <label htmlFor="password_confirm">Password confirmation</label> */}
-					<input
-						type="password"
-						name="password_confirm"
-						id="password_confirm"
-						placeholder="confirm password"
-						required
-					/>
-					<button type="submit">REGISTER</button>
-				</form>
-			</div>
-		</div>
+		<>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<div className={styles.layout}>
+					<div className={styles.auth_container}>
+						<h1>
+							Hi! I'm plant{" "}
+							<img src="../../../assets/images/page-main/plant-11.png" />,
+							<br></br>
+							let introduce yourself!
+						</h1>
+						<form
+							onSubmit={handleSubmit}
+							className={styles.auth_form}
+						>
+							{/* <label htmlFor="firstName">Name</label> */}
+							<input
+								type="text"
+								name="firstName"
+								id="firstName"
+								placeholder="what's your name?"
+								required
+							/>
+							{/* <label htmlFor="lastName">Lastname</label> */}
+							<input
+								type="text"
+								name="lastName"
+								id="lastName"
+								placeholder="what's your lastname?"
+								required
+							/>
+							{/* <label htmlFor="email">Email</label> */}
+							<input
+								type="email"
+								name="email"
+								id="email"
+								placeholder="enter your email"
+								required
+							/>
+							{/* <label htmlFor="password">Password</label> */}
+							<input
+								type="password"
+								name="password"
+								id="password"
+								placeholder="create password"
+								required
+							/>
+							{/* <label htmlFor="password_confirm">Password confirmation</label> */}
+							<input
+								type="password"
+								name="password_confirm"
+								id="password_confirm"
+								placeholder="confirm password"
+								required
+							/>
+							<button type="submit">REGISTER</button>
+						</form>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
