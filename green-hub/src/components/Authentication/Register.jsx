d62b49b1a126dc8e1d78480eb4418ad550bbe57c
Michał Ostrowski <ostmic@gmail.com>
2023-07-02 08:55:52 +0200

    favicon changed, Loader updated

--- green-hub/src/components/Authentication/Register.jsx
+++ green-hub/src/components/Authentication/Register.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "./Authentication.module.css";
 import { toast } from "react-hot-toast";
 import useAuth from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { setDoc, doc } from "@firebase/firestore";
 import { db } from "../../config/firebase";
-import React from "react";
 
 const Register = () => {
 	const navigate = useNavigate();
@@ -43,55 +43,59 @@ const Register = () => {
 
 	return (
 		<div className={styles.layout}>
-		<div className={styles.auth_container}>
-			<h1>Hi! I'm plant <img src="../../../assets/images/page-main/plant-11.png"/>,<br></br>let introduce yourself!</h1>
-			<form
-				onSubmit={handleSubmit}
-				className={styles.auth_form}
-			>
-				{/* <label htmlFor="firstName">Name</label> */}
-				<input
-					type="text"
-					name="firstName"
-					id="firstName"
-					placeholder="what's your name?"
-					required
-				/>
-				{/* <label htmlFor="lastName">Lastname</label> */}
-				<input
-					type="text"
-					name="lastName"
-					id="lastName"
-					placeholder="what's your lastname?"
-					required
-				/>
-				{/* <label htmlFor="email">Email</label> */}
-				<input
-					type="email"
-					name="email"
-					id="email"
-					placeholder="enter your email"
-					required
-				/>
-				{/* <label htmlFor="password">Password</label> */}
-				<input
-					type="password"
-					name="password"
-					id="password"
-					placeholder="create password"
-					required
-				/>
-				{/* <label htmlFor="password_confirm">Password confirmation</label> */}
-				<input
-					type="password"
-					name="password_confirm"
-					id="password_confirm"
-					placeholder="confirm password"
-					required
-				/>
-				<button type="submit">REGISTER</button>
-			</form>
-		</div>
+			<div className={styles.auth_container}>
+				<h1>
+					Hi! I'm plant{" "}
+					<img src="../../../assets/images/page-main/plant-11.png" />,<br></br>
+					let introduce yourself!
+				</h1>
+				<form
+					onSubmit={handleSubmit}
+					className={styles.auth_form}
+				>
+					{/* <label htmlFor="firstName">Name</label> */}
+					<input
+						type="text"
+						name="firstName"
+						id="firstName"
+						placeholder="what's your name?"
+						required
+					/>
+					{/* <label htmlFor="lastName">Lastname</label> */}
+					<input
+						type="text"
+						name="lastName"
+						id="lastName"
+						placeholder="what's your lastname?"
+						required
+					/>
+					{/* <label htmlFor="email">Email</label> */}
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="enter your email"
+						required
+					/>
+					{/* <label htmlFor="password">Password</label> */}
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="create password"
+						required
+					/>
+					{/* <label htmlFor="password_confirm">Password confirmation</label> */}
+					<input
+						type="password"
+						name="password_confirm"
+						id="password_confirm"
+						placeholder="confirm password"
+						required
+					/>
+					<button type="submit">REGISTER</button>
+				</form>
+			</div>
 		</div>
 	);
 };
