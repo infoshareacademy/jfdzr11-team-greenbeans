f9ae1a060c31535e1eec7842b9e50cf8be98e197
PatrykUminski <phantomhd2@gmail.com>
2023-07-09 13:59:40 +0200

    Reinstalled node_modules, Mapping messages and reply done

--- green-hub/src/components/Messages/Messages.jsx
+++ green-hub/src/components/Messages/Messages.jsx
@@ -2,14 +2,19 @@ import Footer from "../Footer/Footer";
 import styles from "../Messages/Messages.module.css";
 import Navbar from "../Navbar/Navbar";
 import { db } from "../../config/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { Fragment, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
 const Messages = () => {
   const messagesCollectionRef = collection(db, "contacts");
   const [messages, setMessages] = useState([]);
-  console.log(messages);
+
+  // WIP
+  // const handleDelete = () => {
+  //   const docRef = doc(db, "contacts", doc.id);
+  //   deleteDoc(docRef);
+  // };
 
   const getMessages = async () => {
     try {
@@ -20,8 +25,9 @@ const Messages = () => {
         Email: doc.data().email,
         Type: doc.data().type,
         Message: doc.data().message,
+        id: doc.id,
       }));
-
+      console.log(filteredData);
       setMessages(filteredData);
     } catch (error) {
       console.error(error);
@@ -35,54 +41,41 @@ const Messages = () => {
 
   return (
     <div className={styles.main_container}>
-      <div className={styles.navbar_container}>
-        <Navbar />
-      </div>
+      <Navbar />
       <div className={styles.messages_container}>
         {messages.length > 0 ? (
-          messages.map((messages) => {
-            <Fragment key={messages.id}>
-              <p>
-                <span>Contact name: {messages.Name}</span>
-                <span>Contact email: {messages.Email}</span>
-                <span>Contact from business/private: {messages.Type}</span>
-                <span>Contact message: {messages.message}</span>
+          messages.map((messages, idx) => (
+            <Fragment key={idx}>
+              <p className={styles.msgpara}>
+                Contact name: {messages.Name} <br />
+                Contact email: {messages.Email} <br />
+                Contact from business/private: {messages.Type} <br />
+                Contact message: {messages.Message}
               </p>
-            </Fragment>;
-          })
+              <button
+                onClick={() => {
+                  window.location = `mailto:${messages.Email}`;
+                }}
+              >
+                Reply
+              </button>
+              {/* <button
+                // WIP
+                onClick={() => {
+                  handleDelete()
+                }}
+              >
+                Delete
+              </button> */}
+            </Fragment>
+          ))
         ) : (
           <p>You dont have any messages yet!</p>
         )}
       </div>
-      <div className={styles.footer_container}>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
 
 export default Messages;
-
-// onst[(studentsGrades, setStudentsGrades)] = useState([]);
-
-// const studentsCollectionRef = collection(db, "students");
-
-// const getStudents = async () => {
-//   try {
-//     const data = await getDocs(studentsCollectionRef);
-
-//     const filteredData = data.docs.map((doc) => ({
-//       studentName: doc.data().name + " " + doc.data().lastName,
-//       grades: doc.data().grades,
-//       id: doc.id,
-//     }));
-
-//     setStudentsGrades(filteredData);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// useEffect(() => {
-//   getStudents();
-// }, []);
