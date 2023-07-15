2f90ab16874012c8f220c59443d7a614906d6ad0
Bartlomiej Galicki <bartlomiej.galicki.sp@lhsystems.com>
2023-07-15 10:02:40 +0200

    fix animation logic for Ideas comp

--- green-hub/src/components/Ideas/Ideas.jsx
+++ green-hub/src/components/Ideas/Ideas.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState, createRef } from "react";
 import { Navbar, Footer } from "../index";
 import { db } from "../../config/firebase.js";
 import {
@@ -14,19 +14,18 @@ import {
 import { Toaster, toast } from "react-hot-toast";
 import IdeaCard from "../IdeaCard/IdeaCard";
 import styles from "./Ideas.module.css";
+import "./Ideas.css";
 import useAuth from "../../context/AuthContext";
-import {TransitionGroup, CSSTransition} from "react-transition-group"
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const Ideas = () => {
-
-	const nodeRef = useRef(null)
 	const [idea, setIdea] = useState([]);
 	const [user, setUser] = useState("");
 	const ideasCollectionRef = collection(db, "ideas");
 	const { currentUser } = useAuth();
 
 	// POBIERANIE USERÓW
-		const getUserName = async () => {
+	const getUserName = async () => {
 		try {
 			const userData = await getDoc(doc(db, "users", currentUser?.uid));
 			const userName = `${userData.data().name} ${userData.data().lastName}`;
@@ -49,6 +48,7 @@ const Ideas = () => {
 	const getIdeasFromSnapshot = (querySnapshot) => {
 		return querySnapshot.docs.map((doc) => ({
 			id: doc.id,
+			nodeRef: createRef(null),
 			...doc.data(),
 		}));
 	};
@@ -56,7 +56,9 @@ const Ideas = () => {
 	const dbListener = (cb) => onSnapshot(q, cb);
 
 	useEffect(() => {
-		dbListener((querySnapshot) => setIdea(getIdeasFromSnapshot(querySnapshot)));
+		dbListener((querySnapshot) =>
+			setIdea(getIdeasFromSnapshot(querySnapshot))
+		);
 	}, []);
 
 	// funkcja do stworzenia obiektu z nowym pomysłem przesłanym przez użytkownika
@@ -68,6 +70,7 @@ const Ideas = () => {
 			auth: currentUser.uid,
 			totalLikes: 0,
 			usersLikes: [],
+			nodeRef: createRef(null),
 		};
 		e.target.reset();
 		return newIdea;
@@ -93,80 +96,106 @@ const Ideas = () => {
 		}
 	};
 
-  return (
-    <div className={styles.ideas}>
-      <Toaster />
-      <Navbar />
-      <main>
-        <h1>ideas</h1>
-        <p>
-          Hi! It's time to introduce ourselves!<br></br>We are an association
-          that wants to make people aware of the importance of taking care of
-          our planet and how much we threaten its health.<br></br>To encourage
-          users to take action, we have partnered with large companies, as you
-          can see in the Prize section. To learn more about us, click About.
-        </p>
-        <p>
-          We are certain that activities we have proposed are great.<br></br>But
-          we are also sure that you have great ideas too, share them here.
-          <br></br>If other users also express a desire to take care of the
-          planet in this way, we will consider adding this activity in the
-          Points section.
-        </p>
-        <div className={styles.paraWithImg}>
-        <div className={styles.imagesPack}>
-          <img src="../../../assets/images/page-ideas/tree-12.png" alt="" />
-          <img src="../../../assets/images/page-ideas/tree-11.png" alt="" />
-          <img src="../../../assets/images/page-ideas/bush.png" alt="" />
-        </div>
-        <p>On behalf of plants, animals and the whole earth - THANK YOU!</p>
-        <div className={styles.imagesPack}>
-          <img src="../../../assets/images/page-ideas/rabbit.png" alt="" />
-          <img src="../../../assets/images/page-ideas/fox.png" alt="" />
-          <img src="../../../assets/images/page-ideas/dolphin.png" alt="" />
-        </div>
-        </div>
-        <img src="../../../assets/images/page-ideas/save.png" />
-        {currentUser?.uid ? <form onSubmit={handleSubmit} className={styles.submit}>
-          <textarea
-            name="idea"
-            id="idea"
-            type="text"
-            placeholder="type your idea here!.."
-          ></textarea>
-          <button>SUBMIT</button>
-        </form> 
-        : null
-        } 
-		
-        {idea
-          ? <ul><TransitionGroup>{idea.map((idea) => {
-              const date = idea?.date?.toDate().toDateString();
-              return (
-				<CSSTransition timeout={500} classNames={styles.item} nodeRef={nodeRef}>
-					<li ref={nodeRef}>
-					<IdeaCard
-                  key={idea.id}
-                  id={idea.id}
-                  user={idea.user}
-                  date={date}
-                  idea={idea.idea}
-                  auth={idea.auth}
-                  totalLikes={idea.totalLikes}
-                  usersLikes={idea.usersLikes}
-                />
-					</li>
-                
-				</CSSTransition>
-				
-              );
-            })}</TransitionGroup></ul>
-          : null}
-		
-      </main>
-      <Footer />
-    </div>
-  );
+	console.log("idea ", idea);
+
+	return (
+		<div className={styles.ideas}>
+			<Toaster />
+			<Navbar />
+			<main>
+				<h1>ideas</h1>
+				<p>
+					Hi! It is time to introduce ourselves!<br></br>We are an
+					association that wants to make people aware of the importance of
+					taking care of our planet and how much we threaten its health.
+					<br></br>To encourage users to take action, we have partnered
+					with large companies, as you can see in the Prize section. To
+					learn more about us, click About.
+				</p>
+				<p>
+					We are certain that activities we have proposed are great.
+					<br></br>But we are also sure that you have great ideas too,
+					share them here.
+					<br></br>If other users also express a desire to take care of the
+					planet in this way, we will consider adding this activity in the
+					Points section.
+				</p>
+				<div className={styles.paraWithImg}>
+					<div className={styles.imagesPack}>
+						<img
+							src="../../../assets/images/page-ideas/tree-12.png"
+							alt=""
+						/>
+						<img
+							src="../../../assets/images/page-ideas/tree-11.png"
+							alt=""
+						/>
+						<img
+							src="../../../assets/images/page-ideas/bush.png"
+							alt=""
+						/>
+					</div>
+					<p>
+						On behalf of plants, animals and the whole earth - THANK YOU!
+					</p>
+					<div className={styles.imagesPack}>
+						<img
+							src="../../../assets/images/page-ideas/rabbit.png"
+							alt=""
+						/>
+						<img src="../../../assets/images/page-ideas/fox.png" alt="" />
+						<img
+							src="../../../assets/images/page-ideas/dolphin.png"
+							alt=""
+						/>
+					</div>
+				</div>
+				<img src="../../../assets/images/page-ideas/save.png" />
+				{currentUser?.uid ? (
+					<form onSubmit={handleSubmit} className={styles.submit}>
+						<textarea
+							name="idea"
+							id="idea"
+							type="text"
+							placeholder="type your idea here!.."
+						></textarea>
+						<button>SUBMIT</button>
+					</form>
+				) : null}
+
+				{idea ? (
+					<ul>
+						<TransitionGroup>
+							{idea.map((idea) => {
+								const date = idea?.date?.toDate().toDateString();
+								return (
+									<CSSTransition
+										key={idea.id}
+										timeout={500}
+										classNames="item"
+										nodeRef={idea.nodeRef}
+									>
+										<li ref={idea.nodeRef}>
+											<IdeaCard
+												id={idea.id}
+												user={idea.user}
+												date={date}
+												idea={idea.idea}
+												auth={idea.auth}
+												totalLikes={idea.totalLikes}
+												usersLikes={idea.usersLikes}
+											/>
+										</li>
+									</CSSTransition>
+								);
+							})}
+						</TransitionGroup>
+					</ul>
+				) : null}
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Ideas;
