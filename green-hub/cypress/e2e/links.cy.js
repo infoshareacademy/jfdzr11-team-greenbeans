d5897f942b99312ad741e5d78930be2855a595d9
Michał Ostrowski <ostmic@gmail.com>
2023-07-16 11:16:19 +0200

    Cypress tests - url fix

--- green-hub/cypress/e2e/links.cy.js
+++ green-hub/cypress/e2e/links.cy.js
@@ -1,30 +1,30 @@
 // Testy  buttonów na głównej stronie
 describe("Home page inks check", () => {
 	it("Should visit each home page button link and go back to home page", () => {
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 
 		cy.contains(/^Articles$/).click();
 		cy.get("div").find("h1").first("h1").should("contain", "articles");
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 
 		cy.contains(/^Get points!$/).click();
 		cy.get("div").find("h1").first("h1").should("contain", "points");
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 
 		cy.contains(/^New ideas$/).click();
 		cy.get("div").find("h1").first("h1").should("contain", "ideas");
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 
 		cy.contains(/^Prizes$/).click();
 		cy.get("div").find("h1").first("h1").should("contain", "prize");
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 	});
 });
 
 //Testy linków w Navbar
 describe("Navbar links check", () => {
 	it("Should visit each Navbar link and go back to home page", () => {
-		cy.visit(" http://localhost:5173/");
+		cy.visit("http://localhost:5173/jfdzr11-team-greenbeans/#/");
 
 		cy.contains(/^Articles$/).click();
 		cy.get('[title="Points"] > img').click();
@@ -37,6 +37,6 @@ describe("Navbar links check", () => {
 		cy.get("h1").should("contain", "articles");
 
 		cy.get('[title="Home"]').click();
-		cy.url().should("eq", "http://localhost:5173/");
+		cy.url().should("eq", "http://localhost:5173/jfdzr11-team-greenbeans/#/");
 	});
 });
